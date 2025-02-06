a5ca2426131187c2ec31956d3ec4a93b5a581ec8
Odra Sanchez <sanchez.00dra@gmail.com>
2025-02-06 04:12:09 -0600

    Tarea5-5-getbookreview

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -66,8 +66,18 @@ public_users.get('/title/:title', function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+
+  if (book) {
+    if (Object.keys(book.reviews).length > 0) {
+      res.status(200).json(book.reviews);
+    } else {
+      res.status(404).json({ message: "No reviews available for this book" });
+    }
+  } else {
+    res.status(404).json({ message: "Book not found" });
+  }
 });
 
 module.exports.general = public_users;
