f302358b8be5289c18c6aaef8c44b785feabb229
Odra Sanchez <sanchez.00dra@gmail.com>
2025-02-06 13:20:00 -0600

    Tarea8-8-reviewadded

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -44,8 +44,32 @@ regd_users.post("/login", (req, res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+ if (!req.session.authorization) {
+  return res.status(403).json({ message: "User not authenticated" });
+}
+
+const isbn = req.params.isbn;
+const book = books[isbn];
+
+if (!book) {
+  return res.status(404).json({ message: "Book not found" });
+}
+
+const { review } = req.body;
+if (!review) {
+  return res.status(400).json({ message: "Review content is required" });
+}
+
+const user = req.session.authorization.username;
+if (book.reviews[user]) {
+  return res.status(400).json({ message: "User already reviewed this book" });
+}
+
+book.reviews[user] = review;
+console.log(`Reseña agregada correctamente para el libro con ISBN ${isbn}:`, book.reviews);
+
+// Responder con el libro actualizado
+return res.status(200).json({ message: "Review added successfully", book });
 });
 
 module.exports.authenticated = regd_users;
