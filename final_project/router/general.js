9eafa768c456df4fcd1c497309649f877c4fd063
Odra Sanchez <sanchez.00dra@gmail.com>
2025-02-06 04:06:12 -0600

    Tarea4-4-getbooksbytitle

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -48,8 +48,20 @@ public_users.get('/author/:author', function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const title = req.params.title.toLowerCase(); 
+ 
+  const result = [];
+  for (let key in books) {
+    if (books[key].title.toLowerCase() === title) {
+      result.push(books[key]);
+    }
+  }
+
+  if (result.length > 0) {
+    res.status(200).json(result);
+  } else {
+    res.status(404).json({ message: "Title not found" });
+  }
 });
 
 //  Get book review
